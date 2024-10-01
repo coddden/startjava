3a0c152759b6a9b8ceef14e044f6c1ae2c6c2264
Denis <coddden@gmail.com>
2024-10-01 21:05:04 +0500

    Calculator v2

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,43 +1,33 @@
 public class Calculator {
     public static void main(String[] args) {
-        int clientFirstNum = 2;
-        int clientSecondNum = 10;
-        char clientMathSymbol = '^';
-        int answer = 0;
+        int firstNum = 2;
+        int secondNum = 10;
+        char mathSign = '^';
+        int result = 0;
         boolean isComputed = false;
-        if (clientFirstNum >= 0 && clientSecondNum >= 0) {
-            if (clientMathSymbol == '+' || clientMathSymbol == '-' || clientMathSymbol == '*' ||
-                    clientMathSymbol == '/' || clientMathSymbol == '%' || clientMathSymbol == '^') {
+        if (firstNum >= 0 && secondNum >= 0) {
+            if (mathSign == '+' || mathSign == '-' || mathSign == '*' ||
+                    mathSign == '/' || mathSign == '%' || mathSign == '^') {
                 isComputed = true;
-                if (clientMathSymbol == '+') {
-                    answer = clientFirstNum + clientSecondNum;
-                } else if (clientMathSymbol == '-') {
-                    if (clientFirstNum >= clientSecondNum) {
-                        answer = clientFirstNum - clientSecondNum;
-                    } else {
-                        isComputed = false;
-                        System.out.printf("%nПервый операнд должен быть больше второго%n");
-                    }
-                } else if (clientMathSymbol == '*') {
-                    answer = clientFirstNum * clientSecondNum;
-                } else if (clientMathSymbol == '/') {
-                    if (clientFirstNum % clientSecondNum == 0) {
-                        answer = clientFirstNum / clientSecondNum;
+                if (mathSign == '+') {
+                    result = firstNum + secondNum;
+                } else if (mathSign == '-') {
+                    result = firstNum - secondNum;
+                } else if (mathSign == '*') {
+                    result = firstNum * secondNum;
+                } else if (mathSign == '/') {
+                    if (firstNum % secondNum == 0) {
+                        result = firstNum / secondNum;
                     } else {
                         isComputed = false;
                         System.out.printf("%nКалькулятор работает только с натуральными числами%n");
                     }
-                } else if (clientMathSymbol == '%') {
-                    if (clientFirstNum > clientSecondNum) {
-                        answer = clientFirstNum % clientSecondNum;
-                    } else {
-                        isComputed = false;
-                        System.out.printf("%nПервый операнд должен быть больше второго%n");
-                    }
-                } else if (clientMathSymbol == '^') {
-                    answer = 1;
-                    for (int i = 0; i < clientSecondNum; i++) {
-                        answer *= clientFirstNum;
+                } else if (mathSign == '%') {
+                    result = firstNum % secondNum;
+                } else if (mathSign == '^') {
+                    result = 1;
+                    for (int i = 0; i < secondNum; i++) {
+                        result *= firstNum;
                     }
                 }
             } else {
@@ -48,7 +38,7 @@ public class Calculator {
         }
         if (isComputed) {
             System.out.printf("%n%d %c %d = %d%n",
-                    clientFirstNum, clientMathSymbol, clientSecondNum, answer);
+                    firstNum, mathSign, secondNum, result);
         }
     }
 }
