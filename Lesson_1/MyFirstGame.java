8344a18eaf8108005956aeafa210e050e78d8127
Denis <coddden@gmail.com>
2024-10-03 13:49:22 +0500

    MyFirstGame v2

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -4,20 +4,20 @@ public class MyFirstGame {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int coumputerNum = 49;
-        int clientNum = 0;
+        int playerNum = 0;
         System.out.printf("%nПривет! Отгадай число от 1 до 100%n");
-        while (clientNum != coumputerNum) {
-            clientNum = in.nextInt();
-            if (clientNum >= 1 && clientNum <= 100) {
-                if (clientNum == coumputerNum) {
+        while (playerNum != coumputerNum) {
+            playerNum = in.nextInt();
+            if (playerNum >= 1 && playerNum <= 100) {
+                if (playerNum < coumputerNum) {
+                    System.out.printf("%n%d меньше того, что загадал компьютер%n", playerNum);
+                } else if (playerNum > coumputerNum) {
+                    System.out.printf("%n%d больше того, что загадал компьютер%n", playerNum);
+                } else {
                     System.out.println("Вы победили!");
-                } else if (clientNum < coumputerNum) {
-                    System.out.printf("%n%d меньше того, что загадал компьютер%n", clientNum);
-                } else if (clientNum > coumputerNum) {
-                    System.out.printf("%n%d больше того, что загадал компьютер%n", clientNum);
                 }
             } else {
-                System.out.printf("%nЧисло должно быть в диапозоне 1-100%n");
+                System.out.printf("%nЧисло должно быть в диапазоне 1-100%n");
             }
         }
     }
