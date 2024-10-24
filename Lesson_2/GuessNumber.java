b2022ed64e0d3ce7f06284dde0fee585e5ffec77
Denis <coddden@gmail.com>
2024-10-24 10:54:54 +0500

    GuessNumber v2

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -3,42 +3,52 @@ import java.util.Scanner;
 
 public class GuessNumber {
 
-    private int computerNumber;
     private Player player1;
     private Player player2;
+    private int secretNum;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
-    public void changeComputerNumber() {
-        computerNumber = 1 + (int) (Math.random() * 100);
-    }
+    Scanner scan = new Scanner(System.in);
+    Player currentPlayer = player2;
 
     public void play() {
-        Scanner scan = new Scanner(System.in);
-        changeComputerNumber();
-        Player currentPlayer = player2;
+        changeSecretNumber();
+        changeCurrentPlayer();
         do {
-            currentPlayer = currentPlayer == player1 ? player2 : player1;
-            System.out.printf("%nХодит: %s ", currentPlayer.getName());
-            currentPlayer.setNumber(scan.nextInt());
-            if (currentPlayer.getNumber() >= 1 && currentPlayer.getNumber() <= 100) {
-                if (currentPlayer.getNumber() < computerNumber) {
-                    System.out.printf("%n%d меньше того, что загадал компьютер%n",
-                            currentPlayer.getNumber());
-                } else if (currentPlayer.getNumber() > computerNumber) {
-                    System.out.printf("%n%d больше того, что загадал компьютер%n",
-                            currentPlayer.getNumber());
-                }
-            } else {
-                System.out.printf("%nЧисло должно быть в диапазоне 1-100%n");
-            }
-        } while (computerNumber != currentPlayer.getNumber());
+            makeMove();
+            checkNumber();
+        } while (secretNum != currentPlayer.getNumber());
         System.out.printf("%nВыиграл %s!%n", currentPlayer.getName());
-        return;
+    }
+
+    private void changeSecretNumber() {
+        secretNum = 1 + (int) (Math.random() * 100);
+    }
+
+    private void changeCurrentPlayer() {
+        currentPlayer = currentPlayer == player1 ? player2 : player1;
+    }
+
+    private void makeMove() {
+        System.out.printf("%nХодит: %s ", currentPlayer.getName());
+        currentPlayer.setNumber(scan.nextInt());
+    }
+
+    private void checkNumber() {
+        if (currentPlayer.getNumber() >= 1 && currentPlayer.getNumber() <= 100) {
+            if (currentPlayer.getNumber() < secretNum) {
+                System.out.printf("%n%d меньше того, что загадал компьютер%n",
+                        currentPlayer.getNumber());
+            } else if (currentPlayer.getNumber() > secretNum) {
+                System.out.printf("%n%d больше того, что загадал компьютер%n",
+                        currentPlayer.getNumber());
+            }
+        } else {
+            System.out.printf("%nЧисло должно быть в диапазоне 1-100%n");
+        }
     }
 }
-
-
