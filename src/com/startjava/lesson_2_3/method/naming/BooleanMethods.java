e3caeefc12c0fded78a4daf3f1c9117d62abe957
Denis <coddden@gmail.com>
2024-11-04 15:44:52 +0500

    remove Lesson_1_2

--- src/com/startjava/lesson_2_3/method/naming/BooleanMethods.java
+++ src/com/startjava/lesson_2_3/method/naming/BooleanMethods.java
@@ -1,3 +1,5 @@
+package com.startjava.lesson_2_3.method.naming;
+
 public class BooleanMethods {
     public boolean shouldContinue() {
         System.out.print("\n\n\n" + MethodName.getCurrent() +
