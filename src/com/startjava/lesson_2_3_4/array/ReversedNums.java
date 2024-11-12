4d085290f4ffaf8c591bb38462ea21c0dec25257
Denis <coddden@gmail.com>
2024-11-12 19:52:59 +0500

    ReversedNums

--- src/com/startjava/lesson_2_3_4/array/ReversedNums.java
+++ src/com/startjava/lesson_2_3_4/array/ReversedNums.java
@@ -1,6 +1,6 @@
 package com.startjava.lesson_2_3_4.array;
 
-public class NumArrays {
+public class ReversedNumArray {
     public static void main(String[] args) {
         int[] emptyArray = {};
         reverse(emptyArray);
