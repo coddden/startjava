b7411fcaf4aec7f85929428ad590228df6dde005
Denis <coddden@gmail.com>
2024-11-20 15:19:07 +0500

    ElementRemover v2

--- src/com/startjava/lesson_2_3_4/array/ElementRemover.java
+++ src/com/startjava/lesson_2_3_4/array/ElementRemover.java
@@ -4,49 +4,56 @@ public class ElementRemover {
     public static void main(String[] args) {
         int[] limiters = {-1, 15, 0, 14};
         for (int limiter : limiters) {
-            double[] srcArray = generateArray();
-            double[] modifiedArray = removeExcessElements(limiter, srcArray);
+            float[] srcArray = generateArray();
+            float[] modifiedArray = removeExcessElements(limiter, srcArray);
             int zeroedCells = countZeroedCells(modifiedArray);
-            showArrays(limiter, zeroedCells, srcArray, modifiedArray);
+            displayArrays(limiter, zeroedCells, srcArray, modifiedArray);
         }
     }
 
-    private static double[] generateArray() {
-        double[] srcArray = new double[15];
+    private static float[] generateArray() {
+        float[] srcArray = new float[15];
         for (int i = 0; i < srcArray.length; i++) {
-            srcArray[i] = Math.random();
+            srcArray[i] = (float) Math.random();
         }
         return srcArray;
     }
 
-    private static double[] removeExcessElements(int limiter, double[] srcArray) {
+    private static float[] removeExcessElements(int limiter, float[] srcArray) {
         int len = srcArray.length;
-        double[] modifiedArray = new double[len];
+        if (limiter < 0 || limiter >= len) return srcArray;
+        float[] modifiedArray = new float[len];
         for (int i = 0; i < len; i++) {
-            modifiedArray[i] = i > limiter ? 0 : srcArray[i];
+            modifiedArray[i] = srcArray[limiter] < srcArray[i] ? 0 : srcArray[i];
         }
         return modifiedArray;
     }
 
-    private static int countZeroedCells(double[] modifiedArray) {
+    private static int countZeroedCells(float[] modifiedArray) {
         int zeroedCells = 0;
-        for (double num : modifiedArray) {
+        for (float num : modifiedArray) {
             if (num == 0) zeroedCells++;
         }
         return zeroedCells;
     }
 
-    private static void showArrays(int limiter, int zeroedCells, double[]... arrays) {
-        String modifiedOrNot = "Исходный";
-        for (double[] array : arrays) {
-            System.out.printf("%n%s массив%s", modifiedOrNot, modifiedOrNot.equals("Исходный") ? "\n" : "");
-            if (modifiedOrNot.equals("Изменённый")) {
+    private static void displayArrays(int limiter, int zeroedCells, float[]... arrays) {
+        String modifiedOrNot = "Исходный массив";
+        for (float[] array : arrays) {
+            int len = array.length;
+            System.out.printf("%n%s%s", modifiedOrNot, modifiedOrNot.equals("Исходный массив") ? "\n" : "");
+            if (!modifiedOrNot.equals("Исходный массив")) {
                 System.out.printf(" | Индекс %d | Обнулённых ячеек %d%n", limiter, zeroedCells);
             }
-            for (int i = 0; i < array.length; i++) {
+            for (int i = 0; i < len; i++) {
                 System.out.printf("%.3f%s", array[i], i == 7 ? "\n" : " ");
             }
-            modifiedOrNot = "Изменённый";
+            if (limiter < 0 || limiter >= len) {
+                modifiedOrNot = "Ошибка! Массив не изменён";
+                System.out.println();
+                continue;
+            }
+            modifiedOrNot = "Изменённый массив";
             System.out.println();
         }
     }
