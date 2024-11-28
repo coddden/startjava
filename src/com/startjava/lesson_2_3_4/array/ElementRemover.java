c435f39e195d756187b3f87fbb6ae64e54da3610
Denis <coddden@gmail.com>
2024-11-28 15:31:54 +0500

    ElementRemover v4

--- src/com/startjava/lesson_2_3_4/array/ElementRemover.java
+++ src/com/startjava/lesson_2_3_4/array/ElementRemover.java
@@ -5,58 +5,53 @@ public class ElementRemover {
         int[] indexes = {-1, 15, 0, 14};
         int srcArrayLength = 15;
         for (int index : indexes) {
-            if (!isValidIndex(index, srcArrayLength)) continue;
-            float[] srcArray = generateArray(srcArrayLength);
-            float[] modifiedArray = removeExceedElements(index, srcArray);
-            int zeroedCells = countZeroedCells(modifiedArray);
-            displayArrays(index, zeroedCells, srcArray, modifiedArray);
+            displayResult(index, srcArrayLength,
+                    removeExceedElements(index, generateArray(index, srcArrayLength)));
         }
     }
 
-    private static boolean isValidIndex(int index, int arrayLength) {
-        if (index >= 0 && index < arrayLength) return true;
-        System.out.printf("%nОшибка! Индекс не найден!%n");
-        return false;
-    }
-
-    private static float[] generateArray(int srcArrayLength) {
-        float[] array = new float[srcArrayLength];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (float) Math.random();
+    private static float[] generateArray(int index, int srcArrayLength) {
+        if (index < 0 || index >= srcArrayLength) {
+            System.out.printf("%nОшибка! Индекса %d в массиве не существует!%nДоступные индексы 0-%d%n",
+                    index, srcArrayLength - 1);
+            return null;
         }
-        return array;
+        float[] srcArray = new float[srcArrayLength];
+        for (int i = 0; i < srcArray.length; i++) {
+            srcArray[i] = (float) Math.random();
+        }
+        displayArray("Исходный", srcArray);
+        return srcArray;
     }
 
-    private static float[] removeExceedElements(int index, float[] array) {
+    private static void displayArray(String modifiedOrNot, float[] array) {
+        int len = array.length;
+        System.out.printf("%n%s массив%n", modifiedOrNot);
+        for (int i = 0; i < len; i++) {
+            System.out.printf("%.3f%s", array[i], i == 7 ? "\n" : " ");
+        }
+        System.out.println();
+    }
+
+    private static int removeExceedElements(int index, float[] array) {
+        int zeroedCells = 0;
+        if (array == null) return zeroedCells;
         int len = array.length;
         float[] modifiedArray = new float[len];
         for (int i = 0; i < len; i++) {
-            modifiedArray[i] = array[index] < array[i] ? 0 : array[i];
+            if (array[i] > array[index]) {
+                modifiedArray[i] = 0;
+                zeroedCells++;
+            } else {
+                modifiedArray[i] = array[i];
+            }
         }
-        return modifiedArray;
-    }
-
-    private static int countZeroedCells(float[] array) {
-        int zeroedCells = 0;
-        for (float num : array) {
-            if (num == 0) zeroedCells++;
-        }
+        displayArray("Изменённый", modifiedArray);
         return zeroedCells;
     }
 
-    private static void displayArrays(int index, int zeroedCells, float[]... arrays) {
-        String modifiedOrNot = "Исходный массив";
-        for (float[] array : arrays) {
-            int len = array.length;
-            System.out.printf("%n%s%s", modifiedOrNot, modifiedOrNot.equals("Исходный массив") ? "\n" : "");
-            if (modifiedOrNot.equals("Изменённый массив")) {
-                System.out.printf(" | Индекс %d | Обнулённых ячеек %d%n", index, zeroedCells);
-            }
-            for (int i = 0; i < len; i++) {
-                System.out.printf("%.3f%s", array[i], i == 7 ? "\n" : " ");
-            }
-            modifiedOrNot = "Изменённый массив";
-            System.out.println();
-        }
+    private static void displayResult(int index, int srcArrayLength, int zeroedCells) {
+        if (index < 0 || index >= srcArrayLength) return;
+        System.out.printf("Индекс %d | Обнулённых ячеек %d%n", index, zeroedCells);
     }
 }
