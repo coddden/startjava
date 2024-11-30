ffd44bb03d955db0a5adf6a1b0a6947ec422c5fc
Denis <coddden@gmail.com>
2024-11-30 22:00:35 +0500

    TriangleOfCharacters v3

--- src/com/startjava/lesson_2_3_4/array/TriangleOfCharacters.java
+++ src/com/startjava/lesson_2_3_4/array/TriangleOfCharacters.java
@@ -2,25 +2,27 @@ package com.startjava.lesson_2_3_4.array;
 
 public class TriangleOfCharacters {
     public static void main(String[] args) {
-        StringBuilder TriangleOfNums = create('0', '9', true);
-        System.out.print(TriangleOfNums == null ? "" : TriangleOfNums);
-
-        StringBuilder TriangleOfSigns = create('/', '!', false);
-        System.out.print(TriangleOfSigns == null ? "" : TriangleOfSigns);
-
-        StringBuilder TriangleOfLetters = create('A', 'J', false);
-        System.out.print(TriangleOfLetters == null ? "" : TriangleOfLetters);
+        if (isLess('0', '9')) System.out.print(create('0', '9', true));
+        if (isLess('/', '!')) System.out.print(create('/', '!', false));
+        if (isLess('A', 'J')) System.out.print(create('A', 'J', false));
     }
 
-    private static StringBuilder create(int start, int end, boolean direction) {
-        if (isGreater(start, end)) return null;
+    private static boolean isLess(int start, int end) {
+        if (start > end) {
+            System.out.printf("%nОшибка: левая граница (%d) > правой (%d)%n%n", start, end);
+            return false;
+        }
+        return true;
+    }
+
+    private static StringBuilder create(int start, int end, boolean asc) {
         int len = end - start + 1;
         char[] characters = new char[len];
         for (int i = 0; i < len; i++) {
-            characters[i] = direction ? (char) start++ : (char) end--;
+            characters[i] = asc ? (char) start++ : (char) end--;
         }
         int charAmount = 1;
-        int offset = characters.length;
+        int offset = len;
         StringBuilder triangle = new StringBuilder();
         for (char character : characters) {
             triangle.append(" ".repeat(offset--));
@@ -28,13 +30,5 @@ public class TriangleOfCharacters {
             charAmount += 2;
         }
         return triangle;
-    }
-
-    private static boolean isGreater(int start, int end) {
-        if (start > end) {
-            System.out.printf("%nОшибка: левая граница (%d) > правой (%d)%n%n", start, end);
-            return true;
-        }
-        return false;
     }
 }
