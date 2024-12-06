5dd15375e61a6659b7fbd57de8e7ab904073872b
Denis <coddden@gmail.com>
2024-12-07 00:27:59 +0500

    UniqueNumsFiller v2

--- src/com/startjava/lesson_2_3_4/array/UniqueNumsFiller.java
+++ src/com/startjava/lesson_2_3_4/array/UniqueNumsFiller.java
@@ -1,27 +1,32 @@
 package com.startjava.lesson_2_3_4.array;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class UniqueNumsFiller {
     public static void main(String[] args) {
-        fillAndDisplay(-30, -10, 23);
-        fillAndDisplay(10, 50, 10);
-        fillAndDisplay(-34, -34, 0);
-        fillAndDisplay(-1, 2, -3);
-        fillAndDisplay(5, -8, 2);
+        ascDisplay(fill(-30, -10, 23), 23);
+        ascDisplay(fill(10, 50, 10), 10);
+        ascDisplay(fill(-34, -34, 0), 0);
+        ascDisplay(fill(-1, 2, -3), -3);
+        ascDisplay(fill(5, -8, 2), 2);
     }
 
-    private static void fillAndDisplay(int start, int end, int rowLen) {
-        double arrayLen = (end - start + 1) * 0.75;
-        if (hasError(start, end, rowLen, (int) arrayLen)) return;
-        int[] uniqueNums = new int[(int) arrayLen];
+    private static int[] fill(int start, int end, int rowLen) {
+        int range = end - start + 1;
+        int arrayLen = (int) (range * 0.75);
+        if (hasError(start, end, rowLen, arrayLen)) return null;
+        int[] uniqueNums = new int[arrayLen];
+        Random random = new Random();
         int index = 0;
         while (index < uniqueNums.length) {
-            int generatedNum = start + (int) (Math.random() * (end - start + 1));
+            int generatedNum = random.nextInt(range) + start;
             if (!hasSameNum(generatedNum, uniqueNums, index)) {
                 uniqueNums[index] = generatedNum;
                 index++;
             }
         }
-        display(uniqueNums, rowLen);
+        return uniqueNums;
     }
 
     private static boolean hasError(int start, int end, int rowLen, int arrayLen) {
@@ -49,18 +54,13 @@ public class UniqueNumsFiller {
         return false;
     }
 
-    private static void display(int[] uniqueNums, int rowLen) {
-        int len = uniqueNums.length;
+    private static void ascDisplay(int[] array, int rowLen) {
+        if (array == null) return;
         System.out.println();
-        for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) {
-                if (uniqueNums[i] > uniqueNums[j]) {
-                    int temp = uniqueNums[i];
-                    uniqueNums[i] = uniqueNums[j];
-                    uniqueNums[j] = temp;
-                }
-            }
-            System.out.printf("%d%s", uniqueNums[i], (i + 1) % rowLen == 0 ? '\n' : " ");
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            System.out.print((i + 1) % rowLen == 0 ? "\n" : " ");
         }
         System.out.println();
     }
