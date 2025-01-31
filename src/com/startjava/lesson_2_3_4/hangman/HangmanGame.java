bf503afcf116eef5e56fbb492f3206c212e937f6
Denis <coddden@gmail.com>
2025-01-31 23:35:50 +0500

    Hangman v1

--- src/com/startjava/lesson_2_3_4/hangman/HangmanGame.java
+++ src/com/startjava/lesson_2_3_4/hangman/HangmanGame.java
@@ -1,5 +1,111 @@
 package com.startjava.lesson_2_3_4.hangman;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class HangmanGame {
 
+    private static final String[] HANGMAN = {
+            "_______",
+            "|     |",
+            "|     @",
+            "|    /|\\",
+            "|    / \\",
+            "| GAME OVER!"
+    };
+    private static final String[] WORDS = {"КЛАСС", "ОБЪЕКТ", "ЦИКЛ", "ДАННЫЕ", "МЕТОД"};
+    private char[] secretLetters;
+    private char[] hiddenLetters;
+    private char[] wrongLetters = {};
+    private int guessedLettersCount = 0;
+    private int attempts = 0;
+
+    public HangmanGame(Random random) {
+        this.secretLetters = chooseSecretLetters(random);
+        this.hiddenLetters = fill(secretLetters.length);
+    }
+
+    public String[] getHangman() {
+        return HANGMAN;
+    }
+
+    public char[] getSecretLetters() {
+        return secretLetters;
+    }
+
+    public void setSecretLetters(Random random) {
+        this.secretLetters = chooseSecretLetters(random);
+    }
+
+    public char[] getHiddenLetters() {
+        return hiddenLetters;
+    }
+
+    public void setHiddenLetters() {
+        this.hiddenLetters = fill(secretLetters.length);
+    }
+
+    public char[] getWrongLetters() {
+        return wrongLetters;
+    }
+
+    public void setWrongLetters() {
+        this.wrongLetters = new char[0];
+    }
+
+    public int getGuessedLettersCount() {
+        return guessedLettersCount;
+    }
+
+    public void setGuessedLettersCount() {
+        this.guessedLettersCount = 0;
+    }
+
+    public int getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts() {
+        this.attempts = 0;
+    }
+
+    public boolean hasSameLetter(char letter, char[] array) {
+        for (char item : array) {
+            if (item == letter) return true;
+        }
+        return false;
+    }
+
+    public void revealLetter(char letter) {
+        for (int i = 0; i < secretLetters.length; i++) {
+            if (letter == secretLetters[i]) {
+                if (!hasSameLetter(letter, hiddenLetters) && attempts > 0) {
+                    attempts--;
+                }
+                hiddenLetters[i] = letter;
+                guessedLettersCount++;
+            }
+        }
+    }
+
+    public void hang(char letter) {
+        attempts++;
+        wrongLetters = Arrays.copyOf(wrongLetters, wrongLetters.length + 1);
+        wrongLetters[wrongLetters.length - 1] = letter;
+        for (int i = 0; i < getAttempts(); i++) {
+            System.out.printf("%n%s", HANGMAN[i]);
+        }
+        System.out.println();
+    }
+
+    private char[] chooseSecretLetters(Random random) {
+        int randomNum = random.nextInt(0, WORDS.length);
+        return WORDS[randomNum].toCharArray();
+    }
+
+    private char[] fill(int len) {
+        char[] array = new char[len];
+        Arrays.fill(array, '*');
+        return array;
+    }
 }
--- src/com/startjava/lesson_2_3_4/hangman/HangmanGameMain.java
+++ src/com/startjava/lesson_2_3_4/hangman/HangmanGameMain.java
@@ -6,60 +6,65 @@ import java.util.Scanner;
 
 public class HangmanGameMain {
     public static void main(String[] args) {
-        String[] hangman = {
-                "_______",
-                "|     |",
-                "|     @",
-                "|    /|\\",
-                "|    / \\",
-                "| GAME OVER!"
-        };
-        int attempts = hangman.length;
-
-        String secretWord = chooseRandomWord();
-        char[] guessedWord = new char[secretWord.length()];
-
-        int turnNum = 1;
-        boolean isGuesed = false;
-        int guessedLettersCount = 0;
-
+        Random random = new Random();
         Scanner scan = new Scanner(System.in);
-        while (turnNum <= attempts && !isGuesed) {
-            Arrays.fill(guessedWord, '*');
-            String mask = Arrays.toString(guessedWord).replaceAll("[\\[\\],]", "");
-            System.out.printf("%n%s%n", mask);
-            char letter = scan.next().charAt(0);
-            boolean hasMatch = false;
-            for (int i = 0; i < secretWord.length(); i++) {
-                if (letter == secretWord.charAt(i)) {
-                    hasMatch = true;
-                    break;
-                }
+        HangmanGame game = new HangmanGame(random);
+
+        String msg = "yes";
+        while (game.getAttempts() < game.getHangman().length && msg.equals("yes")) {
+            System.out.println();
+            displayArray(game.getHiddenLetters());
+            System.out.printf("%nПопыток: %d", game.getHangman().length - game.getAttempts());
+            System.out.printf("%nОшибки: ");
+            displayArray(game.getWrongLetters());
+            System.out.printf("%nВаша буква -> ");
+            char letter = Character.toUpperCase(scan.next().charAt(0));
+
+            if (!isCorrectLetter(game, letter)) {
+                System.out.print(letter < 'А' || letter > 'Я' ?
+                        "Введите русскую букву!\n" :
+                        "Такая буква уже была!\n");
+                continue;
             }
-            if (hasMatch) {
-                for (int i = 0; i < secretWord.length(); i++) {
-                    if (letter == secretWord.charAt(i)) {
-                        guessedLettersCount++;
-                        guessedWord[i] = letter;
-                    }
-                }
+
+            if (game.hasSameLetter(letter, game.getSecretLetters())) {
+                game.revealLetter(letter);
             } else {
-                for (int i = 0; i < turnNum; i++) {
-                    System.out.println(hangman[i]);
-                }
-                turnNum++;
+                game.hang(letter);
             }
-            if (guessedLettersCount == secretWord.length()) {
-                isGuesed = true;
+
+            if (game.getGuessedLettersCount() == game.getSecretLetters().length ||
+                    game.getAttempts() == game.getHangman().length) {
+                System.out.printf(game.getGuessedLettersCount() == game.getSecretLetters().length ?
+                        "%nВы угадали слово - " : "%n%nВы проиграли!%nСекретное слово - ");
+                displayArray(game.getSecretLetters());
+                System.out.println();
+
+                game.setSecretLetters(random);
+                game.setHiddenLetters();
+                game.setWrongLetters();
+                game.setGuessedLettersCount();
+                game.setAttempts();
+
+                String finalMsg = "Хотите продолжить игру? [yes / no]: ";
+                do {
+                    System.out.printf("%n%s", finalMsg);
+                    msg = scan.next().toLowerCase();
+                    if (!(msg.equals("no") || msg.equals("yes"))) {
+                        finalMsg = "Введите корректный ответ [yes / no]: ";
+                    }
+                } while (!(msg.equals("no") || msg.equals("yes")));
             }
         }
-        System.out.println("Хотите продолжить?");
     }
 
-    private static String chooseRandomWord() {
-        String[] randomWords = {"цикл", "класс", "объект", "метод", "переменная", "инкапсуляция"};
-        Random random = new Random();
-        int randomNum = random.nextInt(0, randomWords.length);
-        return randomWords[randomNum];
+    private static void displayArray(char[] array) {
+        System.out.printf("%s", Arrays.toString(array).replaceAll("[\\[\\],]", ""));
+    }
+
+    private static boolean isCorrectLetter(HangmanGame game, char letter) {
+        if (letter < 'А' || letter > 'Я') return false;
+        if (game.hasSameLetter(letter, game.getHiddenLetters())) return false;
+        return !game.hasSameLetter(letter, game.getWrongLetters());
     }
 }
